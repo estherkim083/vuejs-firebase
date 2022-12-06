cfe369d492bbaeac26ec4b1e2799b25726d2dd91
estherkim083 <estherkim083@gmail.com>
2022-12-06 18:10:13 +0900

    Form-Practice

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,13 @@
 <template>
-  <h1>Ninja Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">Click</button>
-  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <Results v-if="showResults" :score="score" />
+  <SignupForm />
 </template>
 
 <script>
-import Block from "./components/Block";
-import Results from "./components/Results";
+import SignupForm from "./components/SignUpForm";
 
 export default {
   name: "App",
-  components: { Block, Results },
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-      score: null,
-      showResults: false,
-    };
-  },
-  methods: {
-    start() {
-      this.delay = 2000 + Math.random() * 5000;
-      this.isPlaying = true;
-      this.showResults = false;
-    },
-    endGame(reactionTime) {
-      this.score = reactionTime;
-      this.isPlaying = false;
-      this.showResults = true;
-    },
-  },
+  components: { SignupForm },
 };
 </script>
 
@@ -41,22 +17,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #444;
+  color: #2c3e50;
   margin-top: 60px;
 }
-button {
-  background: #0faf87;
-  color: white;
-  border: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-  font-size: 16px;
-  letter-spacing: 1px;
-  cursor: pointer;
-  margin: 10px;
-}
-button[disabled] {
-  opacity: 0.2;
-  cursor: not-allowed;
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
